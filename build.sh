06b7f28df90ba3faf0a5684a4ce531850f6892a8
michael.chan <michael.chan@tvb.com>
2018-09-16 13:05:54 +0800

    [new chapter] chapter 2.5 added

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@ cd src
 cat   header.tex       \
       prefix.tex       \
       chapter2.1.tex   \
+      chapter2.2.tex   \
+      chapter2.3.tex   \
+      chapter2.4.tex   \
+      chapter2.5.tex   \
       appendix.tex     \
       ending.tex       > yesterday.tex
 xelatex -syntax=1 yesterday
